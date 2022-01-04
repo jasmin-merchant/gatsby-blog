5fa3a72f333c14b3b808003ccb40b6da1f23b3d7
Jasmin Merchant <54054636+jasmin-merchant@users.noreply.github.com>
2022-01-04 23:38:02 +0530

    Displayed Blog list using Contentful (#11)

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
@@ -8,35 +7,37 @@ const BlogPage = ({ data }) => {
         <Layout pageTitle="My Blog Posts">
             <p>Here are my latest Blog posts:</p>
             {
-                data.allMdx.nodes.map((node) => (
-                    <article key={node.id}>
-                      <h2>
-                          <Link to={`/blog/${node.slug}`}>
-                            {node.frontmatter.title}
-                          </Link>
-                        </h2>
-                      <p>Posted: {node.frontmatter.date}</p>
-                    </article>
-                ))
+                data.allContentfulBlog.edges.map(({node}) => {
+                    return (
+                        <article key={node.id}>
+                            <h2> {node.title} </h2>
+                            {/* <h2> 
+                                <Link to={node.slug}>
+                                    {node.title}
+                                </Link>
+                            </h2> */}
+                            <p>Posted on: {node.date}</p>
+                        </article>
+                    )
+                }
+            )
             }
         </Layout>
     )
 }
 
 export const query = graphql`
-    query {
-        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-            nodes {
-                frontmatter {
-                    date(formatString: "MMMM D, YYYY")
-                    title
-                }
-                id
-                body
-                slug
-            }
+query {
+    allContentfulBlog {
+      edges {
+        node {
+          id
+          title
+          date(formatString: "MMMM DD, YYYY")
         }
+      }
     }
+  }
 `
 
 export default BlogPage
